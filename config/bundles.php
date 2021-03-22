37147097cbf1967fb81f831b6d22cb3d54315566
Fernando Garioli <desenvolvimento5@arcoinformatica.com.br>
2021-03-22 17:41:04 -0300

    first commit

---
+++ config/bundles.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Trikoder\Bundle\OAuth2Bundle\TrikoderOAuth2Bundle::class => ['all' => true],
+];
---
+++ src/Controller/PessoaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Pessoa;
+use App\Service\Serializer\CustomSerializer;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/api/pessoa")
+ */
+class PessoaController extends AbstractController
+{
+
+    /**
+     * @Route("", methods={"POST","HEAD"})
+     */
+    public function create(Request $request)
+    {
+        dd(json_decode($request->getContent()));
+        // $pessoa = $serializer->deserialize($request->getContent(), Pessoa::class);
+        // dump($pessoa);
+        // return new Response($serializer->serialize($pessoa), Response::HTTP_OK, ["Content-Type" => "application/json"]);
+    }
+
+    // /**
+    //  * @Route("/{id}", methods={"PUT"})
+    //  */
+    // public function edit(int $id)
+    // {
+    //     return new Response(json_encode(['id' => $id, 'METHOD' => 'PUT']), Response::HTTP_OK, ["Content-Type" => "application/json"]);
+    // }
+
+    // /**
+    //  * @Route("/{id}", methods={"DELETE"})
+    //  */
+    // public function delete($id)
+    // {
+    //     return new Response(json_encode(['id' => $id, 'METHOD' => 'DELETE']), Response::HTTP_OK, ["Content-Type" => "application/json"]);
+    // }
+
+    // /**
+    //  * @Route("/{id}", methods={"GET","HEAD"})
+    //  */
+    // public function findById(int $id)
+    // {
+    //     // ... return a JSON response with the post
+    //     // throw new Exception("Exceção de teste");
+    //     return new Response(json_encode(['id' => $id]), Response::HTTP_OK, ["Content-Type" => "application/json"]);
+    // }
+
+    // /**
+    //  * @Route("", methods={"GET","HEAD"})
+    //  */
+    // public function findAll(Request $request)
+    // {
+    //     dump($request->query->get('filter'));
+    //     // ... return a JSON response with the post
+    //     // throw new Exception("Exceção de teste");
+    //     return new Response(json_encode(['METHOD' => 'findAll']), Response::HTTP_OK, ["Content-Type" => "application/json"]);
+    // }
+}
